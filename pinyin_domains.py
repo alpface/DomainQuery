3df6ea31556842f2a59e309987bb26599ddbbbef
alpface <seyooe@gmail.com>
2018-08-14 19:44:31 +0800

    Update pinyin query

--- pinyin_domains.py
+++ pinyin_domains.py
@@ -20,8 +20,14 @@ def getPinyinDomains():
 
     # 输入domain后缀
     domain_extension = input("请输入domain后缀，比如：com ")
-    # 需要包含的关键字
+    # 输入拼音的个数：1单拼 2双拼 3三拼 4四拼
+    pinyin_count = int(input("请输入拼音的个数：1单拼 2双拼 3三拼 4四拼"))
+    if pinyin_count <= 0:
+        pinyin_count = 2
+    # 输入包含的关键字
     keycontained = str(input("请输入需要包含的关键字，如果不需要关键字回车即可(不允许包含:空格及其他特殊字符):"))
+    # 输入包含的关键字的类型，1是匹配第一个拼音； 2是匹配中间的拼音； 3是拼配结尾的拼音
+    keycontainedType = int(input("请选择包含的关键字的类型，1是匹配第一个拼音； 2是匹配中间的拼音； 3是拼配结尾的拼音"))
 
     with open('resource/pyim-bigdict.txt', 'r+') as f:
         # 读取所有拼音
@@ -50,11 +56,36 @@ def getPinyinDomains():
                     list = line.split('-')
                     newlist = list
                     if len(list) > 3:
-                        newlist = list[0:3]
+                        newlist = list[0:pinyin_count]
                     new_line = ''
+                    pinyinIdx = 0
+                    qualifiedDomain = True
                     for pinyin in newlist:
+                        # 根据包含的关键字的类型过滤
+                        if keycontainedType == 1:
+                            # 1是匹配第一个拼音
+                            if pinyinIdx == 0 and pinyin != keycontained:
+                                # 不符合就停止
+                                qualifiedDomain = False
+                                break
+                        elif keycontainedType == 2:
+                            # 中间的拼音
+                            if pinyinIdx > 0 and pinyinIdx < len(newlist) - 1:
+                                if pinyin != keycontained:
+                                    qualifiedDomain = False
+                                    break
+                        elif keycontainedType == 3:
+                            # 结尾匹配
+                            if pinyinIdx == len(newlist) - 1:
+                                if pinyin != keycontained:
+                                    qualifiedDomain = False
+                                    break
                         new_line += pinyin
-                    line = new_line
+                        pinyinIdx += 1
+                    if qualifiedDomain == True:
+                        line = new_line
+                    else:
+                        continue
 
                     # 根据keycontained关键字筛选, 不包含关键字的过滤掉
                     if len(keycontained) and keycontained not in line:
