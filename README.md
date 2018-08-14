e642d37ac02ed8dde9d870b02376eb61e325a223
alpface <seyooe@gmail.com>
2018-08-14 13:17:17 +0800

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Query domain form aliyun(wanwang)
+
+### setup
+
+- 1.执行 `main.py`，输入需要查询的类型: 1(四字母) 2(拼音) 3 (单词)
+- 2.根据输入查询的类型提示操作，比如输入的是2，会再次提示输入domain后缀，比如：com，最后输入拼音包含的关键字 
--- pinyin_domains.py
+++ pinyin_domains.py
@@ -20,12 +20,14 @@ def getPinyinDomains():
 
     # 输入domain后缀
     domain_extension = input("请输入domain后缀，比如：com ")
+    # 需要包含的关键字
+    keycontained = str(input("请输入需要包含的关键字，如果不需要关键字回车即可(不允许包含:空格及其他特殊字符):"))
 
     with open('resource/pyim-bigdict.txt', 'r+') as f:
         # 读取所有拼音
         lines = f.readlines()
         print('拼音数据准备中，预计有%s个拼音需要处理' % len(lines))
-        lines = lines[50000: 100000]  # 先查5000个
+        lines = lines[0:20000]  # 先查5000个
 
         with open('resource/query.txt', 'a+') as queryFile:
             # 记录时间
@@ -46,13 +48,17 @@ def getPinyinDomains():
                 # 由于拼音中每个之间都有'-'，所以这里给裁切掉，目前只查询双拼，所以只要两个个拼音
                 if '-' in line:
                     list = line.split('-')
-                    if len(list) > 2:
-                        line = list[0] + list[1]
-                    else:
-                        new_line = ''
-                        for pinyin in list:
-                            new_line += pinyin
-                        line = new_line
+                    newlist = list
+                    if len(list) > 3:
+                        newlist = list[0:3]
+                    new_line = ''
+                    for pinyin in newlist:
+                        new_line += pinyin
+                    line = new_line
+
+                    # 根据keycontained关键字筛选, 不包含关键字的过滤掉
+                    if len(keycontained) and keycontained not in line:
+                        continue
                 else:
                     continue
                 if len(line):
